6fbf47b0ada9f63f1162d6f591da097afb289ee3
yusuffadilahr <yusuffadilah58@gmail.com>
2025-02-04 10:52:43 +0700

    feat: adding DM sans font global

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import HideAppBar from "./_service/clientside/components/headers";
 import RootProvider from "./rootprovider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const dmSans = DM_Sans({
+  variable: '--font-dm-sans',
+  subsets: ['latin'],
+  weight: ["400", "500", "700"]
+})
 
 export const metadata: Metadata = {
   title: "Portofolio | Yusuf Fadilah Rukmana",
@@ -37,7 +33,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${dmSans.className} antialiased`}>
         <RootProvider>
           <HideAppBar />
           <section className="pt-5 px-2">
