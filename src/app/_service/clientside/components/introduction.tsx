401022d2d56f847742f81c0775046f7b3479b0cb
Yusuf Fadilah Rukmana <yusuffadilah58@gmail.com>
2025-03-10 11:22:19 +0700

    backup

--- src/app/_service/clientside/components/introduction.tsx
+++ src/app/_service/clientside/components/introduction.tsx
@@ -2,37 +2,14 @@
 
 import { Box, Typography } from "@mui/material";
 import { useAppSelector } from "../redux";
-// import dynamic from "next/dynamic";
 import * as React from 'react'
-
-// const RotatingText = dynamic(() => import('./reactbits/rotatingtext'))
-// const VariableProximity = dynamic(() => import('./reactbits/varpromixity'))
 
 export default function Introduction() {
     const themeMode = useAppSelector((state) => state.global.themeMode.mode);
-    // const containerRef = React.useRef(null);
     return (
         <>
-            {/* <Box className={`w-full ${themeMode === 'dark' ? 'bg-slate-900 text-white' : 'bg-white border'} 
-            rounded-2xl p-3 sm:p-4 md:p-5 min-h-[2.5rem] h-fit flex justify-center items-center`}>
-                <div ref={containerRef} style={{ position: 'relative' }}>
-                    <VariableProximity label={`Hi, I am Yusuf Fadilah Rukmana`}
-                        className={'variable-proximity-demo'} fromFontVariationSettings="'wght' 400, 'opsz' 9"
-                        toFontVariationSettings="'wght' 1000, 'opsz' 40" containerRef={containerRef}
-                        radius={100} falloff='linear' />
-                </div>
-            </Box> */}
             <Box className={`w-full ${themeMode === 'dark' ? 'bg-slate-900' : 'bg-white border'} 
                 rounded-2xl p-3 sm:p-4 md:p-5 min-h-[2.5rem] h-fit flex justify-center items-center flex-col font-bold`}>
-                {/* <Box className='flex w-full justify-start items-center'>
-                    <Typography className="text-white font-bold text-sm sm:text-base md:text-lg px-1 sm:px-2">As</Typography>
-                    <RotatingText texts={['Front End Developer', 'Back End Developer', 'Fullstack Developer']}
-                        mainClassName="px-1 sm:px-2 md:px-3 bg-cyan-300 text-black font-bold overflow-hidden 
-                        py-0.5 sm:py-1 md:py-2 justify-center rounded-lg text-sm sm:text-base md:text-lg"
-                        staggerFrom="last" initial={{ y: "100%" }} animate={{ y: 0 }}
-                        exit={{ y: "-120%" }} staggerDuration={0.025} splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
-                        transition={{ type: "spring", damping: 30, stiffness: 400 }} rotationInterval={2000} />
-                </Box> */}
                 <Typography className="text-white text-justify"> Passionate web developer with experience in HTML, CSS, JavaScript, and TypeScript. Skilled in building responsive web applications using frameworks like ReactJS and NextJS. Continuously improving through personal projects and staying up-to-date with the latest web development trends.</Typography>
             </Box>
         </>
--- src/app/_service/clientside/components/projectsession.tsx
+++ src/app/_service/clientside/components/projectsession.tsx
@@ -2,6 +2,7 @@
 
 import { Box } from "@mui/material";
 import { useAppSelector } from "../redux";
+import MediaCard from "./card";
 
 export default function ProjectSession() {
     const theme = useAppSelector((state) => state.global.themeMode.mode)
@@ -9,7 +10,7 @@ export default function ProjectSession() {
         <Box className={`w-full h-fit min-h-10 flex py-5 p-2 rounded-2xl
             ${theme === 'dark' ? 'bg-slate-900 text-white' : 'bg-white border border-gray-200'}
         `}>
-
+            <MediaCard />
         </Box>
     );
 }
