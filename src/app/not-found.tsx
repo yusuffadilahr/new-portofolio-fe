889b7d03d4e44462e5cbbf5e47524c6944b6c7c8
Yusuf Fadilah Rukmana <yusuffadilah58@gmail.com>
2025-04-07 05:56:24 +0700

    fix: add button at not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,5 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
+import Link from "next/link";
 
 export default function NotFound() {
     return (
@@ -6,12 +7,21 @@ export default function NotFound() {
             display: 'flex', justifyContent: 'center',
             alignItems: 'center', maxHeight: '100vh',
             maxWidth: '100vw', width: '100%', height: '100%',
-            textAlign: 'center', p: 2
+            textAlign: 'center', p: 2, flexDirection: 'column'
         }}>
             <Typography variant="h5">
                 Opss! This page isn&apos;t available yet.<br />
                 We&apos;re still working on it.
             </Typography>
+            <Link href='/'>
+                <Button variant="contained" sx={{
+                    mt: 2,
+                    textTransform: 'none',
+                    borderRadius: '10px'
+                }} size="small">
+                    Back to home
+                </Button>
+            </Link>
         </Box>
     );
 }
