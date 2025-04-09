e615b677f43db2f651fddf9a775d22421106d85e
Yusuf Fadilah Rukmana <yusuffadilah58@gmail.com>
2025-04-09 11:34:38 +0700

    fix: fixing icon

--- src/app/_service/clientside/components/icons/supabase.tsx
+++ src/app/_service/clientside/components/icons/supabase.tsx
@@ -1,6 +1,6 @@
-export default function SupabaseIcons() {
+export default function SupabaseIcons({ className }: { className?: string }) {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 48 48">
+        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 48 48" className={className}>
             <g id="Ð¡Ð»Ð¾Ð¹_1"><path fill="#71c674" d="M43.9,22.5L27.5,43.3c-1.2,1.4-3.5,0.6-3.5-1.2V30H6.6c-2.1,0-3.3-2.5-2-4.1L20.7,5.1c1.1-1.5,3.4-0.7,3.4,1.2 v12.1h17.7C44.1,18.4,45.3,20.8,43.9,22.5z"></path></g>
         </svg>
     );
--- src/app/_service/clientside/components/techstack.tsx
+++ src/app/_service/clientside/components/techstack.tsx
@@ -12,44 +12,62 @@ import PrismaIcons from "./icons/prisma";
 import MySqlIcons from "./icons/mysql";
 import SupabaseIcons from "./icons/supabase";
 import ReduxIcons from "./icons/redux";
+import Marquee from "react-fast-marquee";
 
 export default function TechStackComponent() {
     const themeMode = useAppSelector((state) => state.global.themeMode.mode)
     return (
+        // <Box className={`w-full ${themeMode === 'dark' ? 'bg-slate-900 text-[#fff]' : 'bg-[#fff] border text-black'} 
+        // rounded-2xl p-3 sm:p-4 md:p-5 min-h-[2.5rem] h-fit flex justify-center items-center font-bold`}>
+        //     <Box className="grid grid-cols-5 w-full gap-4 text-center">
+        //         {/* <Box>
+        //             <TypeScriptIcons className={'text-sm'} />
+        //         </Box>
+        //         <Box>
+        //             <JavaScriptIcons />
+        //         </Box>
+        //         <Box>
+        //             <NodejsIcons />
+        //         </Box>
+        //         <Box>
+        //             <ReactIcons className={'text-xs'} />
+        //         </Box>
+        //         <Box>
+        //             <NextJsIcons />
+        //         </Box>
+        //         <Box>
+        //             <ExpressJsIcons />
+        //         </Box>
+        //         <Box>
+        //             <PrismaIcons />
+        //         </Box>
+        //         <Box>
+        //             <MySqlIcons />
+        //         </Box>
+        //         <Box>
+        //             <SupabaseIcons />
+        //         </Box>
+        //         <Box>
+        //             <ReduxIcons />
+        //         </Box> */}
+        //     </Box>
+        // </Box>
         <Box className={`w-full ${themeMode === 'dark' ? 'bg-slate-900 text-[#fff]' : 'bg-[#fff] border text-black'} 
-        rounded-2xl p-3 sm:p-4 md:p-5 min-h-[2.5rem] h-fit flex justify-center items-center font-bold`}>
-            <Box className="grid grid-cols-5 w-full gap-4 text-center">
-                <Box>
-                    <TypeScriptIcons className={'text-sm'} />
-                </Box>
-                <Box>
-                    <JavaScriptIcons />
-                </Box>
-                <Box>
-                    <NodejsIcons />
-                </Box>
-                <Box>
-                    <ReactIcons className={'text-xs'} />
-                </Box>
-                <Box>
-                    <NextJsIcons />
-                </Box>
-                <Box>
-                    <ExpressJsIcons />
-                </Box>
-                <Box>
-                    <PrismaIcons />
-                </Box>
-                <Box>
-                    <MySqlIcons />
-                </Box>
-                <Box>
-                    <SupabaseIcons />
-                </Box>
-                <Box>
-                    <ReduxIcons />
-                </Box>
-            </Box>
+        // rounded-2xl p-3 sm:p-4 md:p-5 min-h-[2.5rem] h-fit flex justify-center items-center font-bold`}>
+            <Marquee speed={60} gradient={false} direction="left" >
+                <div className="flex items-center gap-3 px-2">
+                    <TypeScriptIcons className="w-10 h-10" />
+                    <JavaScriptIcons className="w-10 h-10" />
+                    <NodejsIcons className="w-10 h-10" />
+                    <ReactIcons className="w-10 h-10" />
+                    <NextJsIcons className="w-10 h-10"/>
+                    <ExpressJsIcons className="w-10 h-10"/>
+                    <PrismaIcons className="w-10 h-10"/>
+                    <MySqlIcons className="w-10 h-10"/>
+                    <SupabaseIcons className="w-10 h-10"/>
+                    <ReduxIcons className="w-10 h-10"/>
+                </div>
+            </Marquee>
         </Box>
     );
 }
